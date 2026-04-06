1627d008362b498a821986b53116392ea84a51c4
kailowren12k <sweetkaimorgan@outlook.com>
2026-04-05 22:28:46 -0500

    Add clientloaded test command

--- src/client/java/com/example/ExampleModClient.java
+++ src/client/java/com/example/ExampleModClient.java
@@ -1,12 +1,31 @@
 package com.example;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommands;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.network.chat.Component;
 
 public class ExampleModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) ->
+			dispatcher.register(
+				ClientCommands.literal("clientloaded")
+					.executes(context -> {
+						context.getSource().sendFeedback(Component.literal("Usage: /clientloaded ping"));
+						return 1;
+					})
+					.then(
+						ClientCommands.literal("ping")
+							.executes(context -> {
+								context.getSource().sendFeedback(Component.literal("Pong from clientloaded"));
+								return 1;
+							})
+					)
+			)
+		);
+
 		ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
 			if (client.player != null) {
 				client.player.sendSystemMessage(Component.literal("Client loaded successfully"));
